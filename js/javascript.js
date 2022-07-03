0e94cc2b833142cb7cc00494d7ea6032a430f8f6
anaflorenciasoria <104400342+anaflorenciasoria@users.noreply.github.com>
2022-07-03 20:30:19 -0300

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,18 @@
+function modooscuro(){
+	document.body.style.backgroundColor = "black";
+	document.getElementById("h1").style.color = "white";
+	document.getElementById("ing").style.color = "white";
+	document.getElementById("sacher").style.color = "white";
+	document.getElementById("proc").style.color = "white";
+	document.getElementById("texto").style.color = "white";
+	document.getElementById("ul").style.color = "white";
+	document.getElementById("ul1").style.color = "white";
+	document.getElementById("ul2").style.color = "white";
+	document.getElementById("texto1").style.color = "white";
+	document.getElementById("texto2").style.color = "white";
+}
+
+function contacto(){
+	document.getElementById("contactoo").style.backgroundColor="white";
+	alert("email:florenciasoria@live.com.ar");
+}
